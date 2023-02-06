ef00fed22c9cf1699c7b8c20c3b54163d96af011
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-06 21:53:20 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 150 ms, Memory: 27,636 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,32 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 import java.util.Arrays;
-import java.util.Scanner;
-
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		int[] arr; int[] L;
 		for (int i = 1; i <= T; i++) {
-			int N = sc.nextInt();
-			int[] arr = new int[N];
-			int[] L = new int[N];
-
-			for (int j = 0; j < N; j++) {
-				arr[j] = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			L = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<N;j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
 			}
-
-			for (int j = 0; j < N; j++) {
-				L[j] = 1;
-				for (int k = 0; k < j; k++) {
-					if (arr[k] <= arr[j]) {
-						L[j] = Math.max(L[k] + 1, L[j]);
+			
+			for(int j=0;j<N;j++) {
+					L[j] = 1;
+				for(int k=0; k<j;k++) {
+					if(arr[k]<=arr[j]) {
+						L[j] = Math.max(L[k]+1, L[j]);
 					}
 				}
 			}
-			Arrays.sort(L);			
-			System.out.println("#" + i + " " + L[N - 1]);
-	
-		}
-		sc.close();
+            Arrays.sort(L);
+			bw.write("#"+i+" "+L[N-1]);
+			bw.flush();
+			bw.newLine();
+		}	
 	}
 }
