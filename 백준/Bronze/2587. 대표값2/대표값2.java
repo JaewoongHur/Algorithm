eb3f386dee75935e0250ea4b6335531bb440b26b
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-07 20:55:06 +0900

    [Bronze II] Title: 대표값2, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[5];
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			a[i] = sc.nextInt();
+			sum += a[i];
+		}
+		sc.close();
+		System.out.println(sum/5);
+		Arrays.sort(a);
+		System.out.println(a[2]);
+		
+	}
+}
