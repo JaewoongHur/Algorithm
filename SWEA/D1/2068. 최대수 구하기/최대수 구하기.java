9499a9087364c178352867b434e75b090ddd3542
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-16 09:50:48 +0900

    [D1] Title: 최대수 구하기, Time: 125 ms, Memory: 20,516 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] arr = new int[10];
+		for(int i=0;i<T;i++) {
+			for(int j=0; j<10;j++) {				
+				arr[j] = sc.nextInt();
+			}
+			Arrays.sort(arr);
+			System.out.println("#"+(i+1)+" "+arr[9]);
+		}
+		sc.close();
+	}
+}
