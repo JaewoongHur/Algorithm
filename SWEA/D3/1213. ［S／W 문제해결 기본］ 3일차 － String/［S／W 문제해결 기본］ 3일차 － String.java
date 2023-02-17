7852f39607b56da5e6085a50a67ea51ea89de9ef
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-17 14:11:44 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 117 ms, Memory: 20,740 KB -BaekjoonHub

---
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for (int i = 1; i <= 10; i++) {
+			int N = Integer.parseInt(br.readLine());
+			String s = br.readLine();
+			String line = br.readLine();
+			int len = s.length();
+			int len2 = line.length();
+			int cnt = 0;
+			for (int j = 0; j <= len2 - len ; j++) {
+				if (s.equals(line.substring(j, j + len))) {
+					cnt++;
+				}
+			}
+			if(s.equals(line.substring(len2-len+1))) {
+				cnt++;
+			}
+			bw.write("#" + N + " " + cnt);
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+	}
+}
