4f45e02513847822232cca3bf8183dd2c695fa2f
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-22 08:47:11 +0900

    [Bronze I] Title: 세로읽기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		char[][] arr = new char[5][16];
+		for (int j = 0; j < 5; j++) {
+			String s = br.readLine();
+			for (int k = 0; k < s.length(); k++) {
+				arr[j][k] = s.charAt(k);
+			}
+			for (int k = s.length(); k < 16; k++) {
+				arr[j][k] = '-';
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int j = 0; j < 16; j++) {
+			for (int k = 0; k < 5; k++) {
+				if (arr[k][j] == '-') {
+					continue;
+				} else {
+					sb = sb.append(arr[k][j]);
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
