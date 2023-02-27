921c0f042bf0419f70ea8269e9b804e919ae0c7f
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-02-27 16:12:41 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 125 ms, Memory: 24,532 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	static int[][] arr;
+	static boolean[][] visit;
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1};
+	static int possible;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= 10; tc++) {
+			int T = Integer.parseInt(br.readLine());
+			arr = new int[100][100];
+			visit = new boolean[100][100];
+			int r1 = 0;
+			int c1 = 0;
+			int r2 = 0;
+			int c2 = 0;
+			possible = 0;
+
+			for (int i = 0; i < 100; i++) {
+				String s = br.readLine();
+				for (int j = 0; j < 100; j++) {
+					arr[i][j] = s.charAt(j) - '0';
+					if (arr[i][j] == 2) {
+						r1 = i;
+						c1 = j;
+					} else if (arr[i][j] == 3) {
+						r2 = i;
+						c2 = j;
+					}
+				}
+			}
+
+			dfs(r1, c1);
+			sb.append('#').append(tc).append(' ').append(possible).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static boolean boundaryCheck(int r, int c) {
+		return r >= 0 && r < 100 && c >= 0 && c < 100;
+	}
+
+	public static void dfs(int r, int c) {
+
+		visit[r][c] = true;
+
+		if (arr[r][c] == 3) {
+			possible = 1;
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (boundaryCheck(nr, nc)) {
+				if (!visit[nr][nc] && (arr[nr][nc] == 0 | arr[nr][nc] == 3)) {
+					dfs(nr, nc);
+				}
+			}
+		}
+
+	}
+
+}
