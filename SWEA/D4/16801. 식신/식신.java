cbf65b66f4a9c1f37346033c8b4b5f0c23fb269d
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-03-12 04:13:49 +0900

    [D4] Title: 식신, Time: 1,214 ms, Memory: 102,824 KB -BaekjoonHub

--- SWEA/D4/16801. 식신/식신.java
+++ SWEA/D4/16801. 식신/식신.java
@@ -6,51 +6,48 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            long K = Long.parseLong(st.nextToken());
-            int[] A = new int[N];
-            int[] F = new int[N];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                A[i] = Integer.parseInt(st.nextToken());
-            }
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                F[i] = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(A);
-            Arrays.sort(F);
-            reverse(F);
-            long left = 0;
-            long right = 0;
-            for (int i = 0; i < N; i++) {
-                right = Math.max(right, (long) A[i] * F[i]);
-            }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			long K = Long.parseLong(st.nextToken());
+			int[] A = new int[N];
+			int[] F = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				A[i] = Integer.parseInt(st.nextToken());
+			}
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				F[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(A);
+			Arrays.sort(F);
+			reverse(F);
+			long left = 0;
+			long right = 9000000000000000000L;
+			while (left < right) {
+				long mid = (left + right) / 2;
+				long cnt = 0;
+				for (int i = 0; i < N; i++) {
+					if (mid < (long) A[i] * F[i]) {
+						cnt += A[i] - mid / F[i];
+					}
+				}
+				if (cnt <= K) {
+					right = mid;
+				} else {
+					left = mid + 1;
+				}
+			}
 
-            while (left < right) {
-                long mid = (left + right) / 2;
-                long cnt = 0;
-                for (int i = 0; i < N; i++) {
-                    if (mid < (long) A[i] * F[i]) {
-                        cnt += A[i] - mid / F[i];
-                    }
-                }
-                if (cnt <= K) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            sb.append("#").append(tc).append(" ").append(left).append("\n");
-        }
-      System.out.println(sb);
+			sb.append("#").append(tc).append(" ").append(left).append("\n");
+		}
+		System.out.println(sb);
 	}
 
 	public static void reverse(int[] arr) {
