ae743a8cb6b3e2d4f182d0c143bc6db174c44dfd
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-03-13 23:14:23 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 127952 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -10,7 +10,6 @@ public class Main {
 	static int[] dc = { 0, 1, 0, -1 };
 	static int N, M, day;
 	static int[][] A;
-	static boolean[][] visit;
 	static Queue<int[]> q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
@@ -19,14 +18,12 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		A = new int[N][M];
-		visit = new boolean[N][M];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
 				if (A[i][j] == 1) {
 					q.offer(new int[] { i, j, 0 });
-					visit[i][j] = true;
 				}
 			}
 		}
@@ -60,7 +57,7 @@ public class Main {
 			for (int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				if (!boundaryCheck(nr, nc) || visit[nr][nc] || A[nr][nc] == -1) {
+				if (!boundaryCheck(nr, nc) ||A[nr][nc] == 1 || A[nr][nc] == -1) {
 					continue;
 				}
 				if (A[nr][nc] == 0) {
