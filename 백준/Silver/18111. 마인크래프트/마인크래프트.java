e7f68e38f0c4fc1d8467057660312a5e939acc83
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-03-22 17:27:17 +0900

    [Silver II] Title: 마인크래프트, Time: 612 ms, Memory: 35208 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int[][] A = new int[N][M];
+		
+		int hmin = Integer.MAX_VALUE;
+		int hmax = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+				hmin = Math.min(hmin, A[i][j]);
+				hmax = Math.max(hmax, A[i][j]);
+			}
+		}		
+		
+		int min = Integer.MAX_VALUE;
+		int maxHeight = 0;
+		int inventory = B;
+		for (int i = hmin; i <= hmax; i++) {
+			int w1 = 0;
+			int w2 = 0;
+			inventory = B;
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < M; k++) {
+					if (i > A[j][k]) {
+							w2 += i - A[j][k];
+						} 
+					else if (i < A[j][k]) {
+							w1 += A[j][k] - i;
+						} 
+					}
+				}
+			if(inventory+w1-w2<0 || inventory+w1-w2>64000000) {
+				continue;
+			}
+			if (min >= w1 * 2 + w2) {
+				min = w1 * 2 + w2;
+				maxHeight = i;
+			}
+		}
+		System.out.println(min+" "+maxHeight);
+	}
+}
