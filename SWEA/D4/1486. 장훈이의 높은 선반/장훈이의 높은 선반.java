30f6156878a8301a8a076d5d6a976561734375d9
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-03-24 10:00:27 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 221 ms, Memory: 22,664 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int N, B, ans;
+	static int[] H;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			H = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				H[i] = Integer.parseInt(st.nextToken());
+			}
+			ans = Integer.MAX_VALUE;
+			for (int i = 0; i < (1 << N); i++) {
+				int sum = 0;
+				for (int j = 0; j < N; j++) {
+					if ((i & (1 << j)) != 0) {
+						sum += H[j];
+					}
+				}
+				if (sum >= B && sum < ans) {
+					ans = sum;
+				}
+			}
+			sb.append("#" + tc + " ").append(ans - B).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
