944ca30e1ee372a3b8f25a4dbfa2e560118b9ad1
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-04-10 12:07:10 +0900

    [Silver II] Title: 이동하기, Time: 424 ms, Memory: 79996 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[N][M];
+		int[][] dp = new int[N+1][M+1];
+		
+		for(int r=0;r<N;r++) {
+			st= new StringTokenizer(br.readLine());
+			for(int c=0;c<M;c++) {
+				arr[r][c] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=M;j++) {
+				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + arr[i-1][j-1] ;
+				
+			}
+		}
+		
+		System.out.println(dp[N][M]);	
+	}
+}
