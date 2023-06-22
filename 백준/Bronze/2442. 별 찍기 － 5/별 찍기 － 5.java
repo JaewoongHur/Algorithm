182d9c4590350f27c5e7d8e1562478f42703df02
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-06-22 18:45:53 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 332 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int r=0;r<n;r++) {
+			for(int c=0;c<n+r;c++) {
+				if(c>=n-1-r) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+}
