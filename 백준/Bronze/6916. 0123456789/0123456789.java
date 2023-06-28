8e34c67870872ffdb5a66e15e3cb92312586a795
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-06-29 07:58:47 +0900

    [Bronze IV] Title: 0123456789, Time: 112 ms, Memory: 12800 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		if(a==0) {
+			System.out.println(" * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");			
+		}
+		else if(a==1) {
+			System.out.println("");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+		}
+		else if(a==2) {
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println(" * * *");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println(" * * *");	
+		}
+		else if(a==3) {
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println(" * * *");
+		}
+		else if(a==4) {
+			System.out.println("");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+		}
+		else if(a==5) {
+			System.out.println(" * * *");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println(" * * *");
+		}
+		else if(a==6) {
+			System.out.println(" * * *");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println("*");
+			System.out.println(" * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");
+		}
+		else if(a==7) {
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+		}
+		else if(a==8) {
+			System.out.println(" * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");	
+		}
+		else if(a==9) {
+			System.out.println(" * * *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println("*     *");
+			System.out.println(" * * *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println("      *");
+			System.out.println(" * * *");
+		}
+	}
+}
