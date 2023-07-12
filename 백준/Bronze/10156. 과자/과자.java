c0cdfc4330abca51e1ba0f5b2ddee27d5fc3162b
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-07-12 12:37:05 +0900

    [Bronze IV] Title: 과자, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(c-a*b>0){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(a*b-c);
+        }
+
+    }
+}
