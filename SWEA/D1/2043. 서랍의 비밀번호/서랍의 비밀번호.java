ed157cece20b062b814ea49b60dcf66cd852ec34
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-09-11 03:14:13 +0900

    [D1] Title: 서랍의 비밀번호, Time: 123 ms, Memory: 20,856 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int k = sc.nextInt();
+		sc.close();
+		System.out.println(p-k+1);
+	}
+}
