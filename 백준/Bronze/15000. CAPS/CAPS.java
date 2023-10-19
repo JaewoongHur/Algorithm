cf2ea72335b0a6b07a826932a0ce15f8a1fd64c3
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-10-19 23:58:43 +0900

    [Bronze V] Title: CAPS, Time: 228 ms, Memory: 23792 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		s = s.toUpperCase();
+		System.out.println(s);
+	}
+}
