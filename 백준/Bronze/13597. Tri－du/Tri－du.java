50620fb4d01b71efa52eb453222a359e75a2a90c
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-10-25 20:47:47 +0900

    [Bronze IV] Title: Tri-du, Time: 80 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		int c = 0;
+
+		if (a == b) {
+			c = a;
+		} else if (a > b) {
+			c = a;
+		} else if (a < b) {
+			c = b;
+		}
+		System.out.println(c);
+	}
+}
