b7c8dd70dbc8d4dd004fda30f26307cbf6881a3b
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2023-12-13 12:16:03 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(" ");
+		
+		int a = Integer.parseInt(s[0]);
+		int b = Integer.parseInt(s[1]);
+		
+		int c = Integer.parseInt(br.readLine());
+		
+		if(2*c > a+b) {
+			System.out.println(a+b);
+		} else {
+			System.out.println(a+b - 2*c);			
+		}	
+	}
+}
