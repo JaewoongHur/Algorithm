d6ddc4efc1b09e62b6216fa0b6a630890b0625c7
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-01-14 11:57:54 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		String s = br.readLine();
+		
+		int cnt = 0;
+		
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u') {
+				++cnt;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
