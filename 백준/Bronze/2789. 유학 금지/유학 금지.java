6e5dd7c1595df9560853350a9bfb9b22b16b9e1c
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-01-15 12:37:12 +0900

    [Bronze II] Title: 유학 금지, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s = br.readLine();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)=='C'||s.charAt(i)=='A'||s.charAt(i)=='M'||s.charAt(i)=='B'||s.charAt(i)=='R'||s.charAt(i)=='I'||s.charAt(i)=='D'||s.charAt(i)=='G'||s.charAt(i)=='E') {
+				continue;
+			}
+			sb.append(s.charAt(i));
+		}
+		System.out.println(sb);
+	}
+}
