8049c5823ed3fe1b06b6e15cc285e29d64f16fd1
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-02-22 10:48:19 +0900

    [Bronze I] Title: N번째 큰 수, Time: 104 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int[] arr = new int[10];
+			
+			for(int i=0;i<10;i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			sb.append(arr[7]).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+}
