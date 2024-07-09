9ba35c5e71dff9ac1a7b8a2e7e4f5011bd861968
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-07-09 14:40:42 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 128 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		int max = m;
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			m += a-b;
+			
+			if(m < 0) {
+				max = 0;
+				break;
+			}
+			
+			max = Math.max(m, max);
+		}
+		System.out.println(max);
+	}
+}
