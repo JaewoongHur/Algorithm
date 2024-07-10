282db82fbc5f3cc11c5dc25258335962946d8aba
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-07-11 08:18:00 +0900

    [Bronze II] Title: 랜덤 숫자 만들기, Time: 456 ms, Memory: 170924 KB -BaekjoonHub

--- 백준/Bronze/6500. 랜덤 숫자 만들기/랜덤 숫자 만들기.java
+++ 백준/Bronze/6500. 랜덤 숫자 만들기/랜덤 숫자 만들기.java
@@ -3,31 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String s;
-        
-        while (true) {
-            s = br.readLine();
-            if (s.equals("0")) {
-                break;
-            }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String s;
+		
+		while (true) {
+			s = br.readLine();
 
-            boolean[] arr = new boolean[10000];
-            int a = Integer.parseInt(s);
-            int cnt = 0;
+			if (s.equals("0")) {
+				break;
+			}
 
-            while (!arr[a]) {
-                arr[a] = true;
-                cnt++;
-                int b = a * a;
-                String c = String.format("%08d", b); // 8자리로 패딩
-                a = Integer.parseInt(c.substring(2, 6));
-            }
-            
-            sb.append(cnt).append('\n');
-        }
-        System.out.println(sb);
-    }
+			boolean[] arr = new boolean[10000];
+
+			int a = Integer.parseInt(s);
+
+			arr[a] = true;
+
+			int b = 0;
+
+			while (true) {
+
+				b = a * a;
+
+				String c = String.valueOf(b);
+
+				while(c.length()<8) {
+					c = "0" + c;
+				}
+				
+				b = Integer.parseInt(c.substring(2, 6));
+				
+				if (arr[b]) {
+					break;
+				}
+
+				arr[b] = true;
+				a = b;
+			}
+
+			int cnt = 0;
+
+			for (int i = 0; i < 10000; i++) {
+				if (arr[i]) {
+					cnt++;
+				}
+			}
+			sb.append(cnt).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
