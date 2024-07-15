f30a0e8315846995b071c40c7494dbb46743fd88
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-07-15 15:19:25 +0900

    [Bronze II] Title: Hard to Believe, but True!, Time: 68 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Bronze/6565. Hard to Believe， but True！/Hard to Believe， but True！.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String s;
+		
+		while(true) {
+			s = br.readLine();
+			
+			if(s.equals("0+0=0")) {
+				System.out.println("True");
+				break;
+			}
+			
+			st = new StringTokenizer(s,"+");
+			
+			String x = st.nextToken();
+			String t = st.nextToken();
+			
+			st = new StringTokenizer(t,"=");
+			String y = st.nextToken();
+			String z = st.nextToken();
+			
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append(x);
+			x = sb.reverse().toString();
+			int a = Integer.parseInt(x);
+			
+			sb = new StringBuilder();
+			sb.append(y);
+			y = sb.reverse().toString();
+			int b = Integer.parseInt(y);
+			
+			sb = new StringBuilder();
+			sb.append(z);
+			z = sb.reverse().toString();
+			int c = Integer.parseInt(z);
+			
+			if(a + b == c) {
+				System.out.println("True");
+			}else {
+				System.out.println("False");
+			}
+		}
+	}
+}
