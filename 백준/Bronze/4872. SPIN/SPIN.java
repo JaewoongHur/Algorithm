19151bb734987963671cf266d00cf3cd90cab77a
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-08-18 11:03:39 +0900

    [Bronze I] Title: SPIN, Time: 60 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/4872. SPIN/SPIN.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s;
+
+		s = br.readLine();
+
+		int n = s.length();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.charAt(i) - '0';
+		}
+		
+		while (true) {
+			s = br.readLine();
+
+			if (s == null) {
+				break;
+			}
+
+			for (int i = 0; i < n; i++) {
+				arr[i] = (arr[i] + s.charAt(i) - '0') % 10;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0;i<n;i++) {
+			sb.append(arr[i]);
+		}
+		System.out.println(sb);
+	}
+}
