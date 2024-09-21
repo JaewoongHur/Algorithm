6394b77e1130f7b60eb99b5f34df205573c92375
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-09-21 14:20:32 +0900

    [Bronze III] Title: Cuboids, Time: 80 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		String s;
+
+		while (true) {
+			s = br.readLine();
+
+			if (s.equals("0 0 0 0")) {
+				break;
+			}
+
+			st = new StringTokenizer(s);
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+
+			if (d == 0) {
+				d = a * b * c;
+			} else if (a == 0) {
+				a = d / b / c;
+			} else if (b == 0) {
+				b = d / a / c;
+			} else if (c == 0) {
+				c = d / a / b;
+			}
+			System.out.println(a + " " + b + " " + c + " " + d);
+		}
+	}
+}
