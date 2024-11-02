b7ac0f49f9f8ee380bfcc539d417950e08c071f0
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-11-02 12:01:49 +0900

    [Bronze III] Title: Ship Selection, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/10180. Ship Selection/Ship Selection.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc=0;tc<T;tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			double d = Double.parseDouble(st.nextToken());
+			
+			int ans = 0;
+			
+			for(int i=0;i<n;i++) {
+				st = new StringTokenizer(br.readLine());
+				
+				double v = Double.parseDouble(st.nextToken());
+				double f = Double.parseDouble(st.nextToken());
+				double c = Double.parseDouble(st.nextToken());
+				
+				if(v*f/c >= d) {
+					ans++;
+				}
+			}
+			System.out.println(ans);
+		}
+	}
+}
