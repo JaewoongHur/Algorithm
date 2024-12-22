f3bd4b81aedbdd52d48857e79f10875cde4c47d7
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2024-12-22 14:53:20 +0900

    [Bronze III] Title: A Serious Reading Problem, Time: 68 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/11368. A Serious Reading Problem/A Serious Reading Problem.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		String s;
+		
+		while(true) {
+			s = br.readLine();
+			
+			if(s.equals("0 0 0 0")) {
+				break;
+			}
+			
+			st = new StringTokenizer(s);
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			
+			long k = (long) Math.pow(a, b*c*d);
+			
+			System.out.println(k);
+		}
+	}
+}
