7009c8c75d8dc8bb700b81ef0db08628a92518ec
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-01-01 07:30:41 +0900

    [Silver V] Title: 지뢰 찾기, Time: 324 ms, Memory: 30088 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.java
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
+		StringBuilder sb = new StringBuilder();
+		
 		int n = Integer.parseInt(br.readLine());
 
 		char[][] map = new char[n][n];
@@ -55,15 +57,16 @@ public class Main {
 		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				System.out.print(ans[i][j]);
+				sb.append(ans[i][j]);
 			}
-			System.out.println();
+			sb.append('\n');
 		}
+		
+		System.out.println(sb);
 
 	}
 
 	private static boolean bound(int r, int c, int n) {
 		return r >= 0 && r < n && c >= 0 && c < n;
 	}
-
 }
