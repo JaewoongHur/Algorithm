fe0de7d6ba876f01ccc42f6d5080b0ce8fbaec62
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-03-27 20:08:34 +0900

    [Bronze II] Title: Chop Cup, Time: 68 ms, Memory: 11456 KB -BaekjoonHub

---
+++ 백준/Bronze/13224. Chop Cup/Chop Cup.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s = br.readLine();
+
+		int a = 1;
+		int b = 0;
+		int c = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'A') {
+				int tmp = b;
+				b = a;
+				a = tmp;
+			} else if (s.charAt(i) == 'B') {
+				int tmp = c;
+				c = b;
+				b = tmp;
+			} else {
+				int tmp = a;
+				a = c;
+				c = tmp;
+			}
+		}
+
+		if (a == 1) {
+			System.out.println(1);
+		} else if (b == 1) {
+			System.out.println(2);
+		} else {
+			System.out.println(3);
+		}
+
+	}
+}
