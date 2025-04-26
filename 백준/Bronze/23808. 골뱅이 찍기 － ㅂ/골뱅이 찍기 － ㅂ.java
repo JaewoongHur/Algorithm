fcf8546ca8c8bb759fdd7dfe194346ff65848573
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-04-26 18:30:38 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 100 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		char[][] arr = new char[5 * n][5 * n];
+
+		for (int i = 0; i < 5 * n; i++) {
+			for (int j = 0; j < 5 * n; j++) {
+				if ((i >= 0 && i < 2 * n) || (i >= 3 * n && i < 4 * n)) {
+					if (j >= n && j < 4 * n) {
+						arr[i][j] = ' ';
+					} else {
+						arr[i][j] = '@';
+					}
+				} else {
+					arr[i][j] = '@';
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 5 * n; i++) {
+			for (int j = 0; j < 5 * n; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+
+	}
+}
