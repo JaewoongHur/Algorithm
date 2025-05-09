2712f23324258adfff3de1f05d95159537e4f1d3
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-05-09 20:41:16 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 84 ms, Memory: 13092 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		char[][] arr = new char[5 * n][5 * n];
+
+		for (int i = 0; i < 5 * n; i++) {
+			for (int j = 0; j < 5 * n; j++) {
+				if (i >= n && i < 5 * n && j >= n && j < 5 * n) {
+					arr[i][j] = ' ';
+				} else {
+					arr[i][j] = '@';
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 5 * n; i++) {
+			for (int j = 0; j < 5 * n; j++) {
+				if(arr[i][j]==' ') continue;
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
