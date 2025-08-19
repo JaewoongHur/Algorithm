e04833eca1372544b034795060d8fda1183a8284
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-08-19 17:46:00 +0900

    [Bronze III] Title: Доставка, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/28812. Доставка/Доставка.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int min = Integer.MAX_VALUE;
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			int k = a + b + c*m;
+			min = Math.min(min, k);
+		}
+		System.out.println(min);
+	}
+}
