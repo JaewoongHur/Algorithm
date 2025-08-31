25d230950c111d075711af8abfd87d5e9e9051d4
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-08-31 12:56:29 +0900

    [Bronze I] Title: Сбор сторонников, Time: 172 ms, Memory: 21540 KB -BaekjoonHub

---
+++ 백준/Bronze/28820. Сбор сторонников/Сбор сторонников.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		long[] arr = new long[n];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+		}
+		
+		long L = 1;
+		
+		for(int i=0;i<n;i++) {
+			L = L/gcd(L,arr[i])*arr[i];
+		}
+		
+		long ans = (s+L)%7;
+		if(ans == 0) ans = 7;
+		
+		System.out.println(ans);
+	}
+	
+	static long gcd(long a, long b) {
+		while(b!=0) {
+			long r = a%b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
