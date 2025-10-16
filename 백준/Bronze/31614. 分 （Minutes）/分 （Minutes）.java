9da6c80ea8becda6a18fe6b92ab224be19c1d7bc
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-10-16 16:34:50 +0900

    [Bronze V] Title: 分 (Minutes), Time: 60 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int h = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		System.out.println(h*60+m);
+
+	}
+}
