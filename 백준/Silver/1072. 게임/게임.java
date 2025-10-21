d3a5c7894c96942d89432601f420f7fa5562b87d
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-10-21 16:44:22 +0900

    [Silver III] Title: 게임, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -8,19 +8,30 @@ public class Main {
 
 		st = new StringTokenizer(br.readLine());
 
-		double x = Double.parseDouble(st.nextToken());
-		double y = Double.parseDouble(st.nextToken());
+		long x = Long.parseLong(st.nextToken());
+		long y = Long.parseLong(st.nextToken());
 
-		double z = Math.floor(y * 100 / x);
+		long z = (y * 100) / x;
 
-		if (z == 100 || z == 99) {
+		if (z >= 99) {
 			System.out.println(-1);
-		} else {
-			if ((x + x * z - 100 * y) % (99 - z) == 0) {
-				System.out.println((long) ((x + x * z - 100 * y) / (99 - z)));
+			return;
+		}
+
+		long left = 1;
+		long right = 1000000000L;
+
+		while (left <= right) {
+			long mid = (left + right) / 2;
+
+			long k = (y + mid) * 100 / (x + mid);
+
+			if (k > z) {
+				right = mid - 1;
 			} else {
-				System.out.println((long) ((x + x * z - 100 * y) / (99 - z)) + 1);
+				left = mid + 1;
 			}
 		}
+		System.out.println(left);
 	}
 }
