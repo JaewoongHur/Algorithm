1f98e736fe0e9e9baa46e85e0e517c4bedf95069
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-11-01 21:39:36 +0900

    [Silver III] Title: 정사각형, Time: 160 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1485. 정사각형/정사각형.java
+++ 백준/Silver/1485. 정사각형/정사각형.java
@@ -2,66 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	int T = Integer.parseInt(br.readLine());
-    	
-    	for(int tc=0;tc<T;tc++) {
-    		
-    		double[][] arr = new double[4][2];
-    		
-    		for(int i=0;i<4;i++) {
-    			String[] s = br.readLine().split(" ");
-    			arr[i][0] = Double.parseDouble(s[0]);
-    			arr[i][1] = Double.parseDouble(s[1]);
-    		}
-    		
-    		
-    		if((arr[0][1]-arr[1][1])*(arr[1][0]-arr[2][0]) == (arr[0][0]-arr[1][0])*(arr[1][1]-arr[2][1])) {
-    			System.out.println(0);
-    			continue;
-    		}
-    		
-    		
-    		int mi = 0;
-    		double md = 0;
-    		
-    		for(int i=1;i<4;i++) {
-    			double d = (arr[0][0]-arr[i][0])*(arr[0][0]-arr[i][0]) + (arr[0][1]-arr[i][1])*(arr[0][1]-arr[i][1]);
-    			if(d > md) {
-    				mi = i;
-    				md = d;
-    			}
-    		}
-    		
-    		int i1 = 0; int i2 = 0;
-    		
-    		for(int i=1;i<4;i++) {
-    			if(i != mi) {
-    				i1 = i;
-    			}
-    		}
-    		
-    		for(int i=1;i<4;i++) {
-    			if(i!=mi && i!=i1) {
-    				i2 = i;
-    			}
-    		}
-    		
-    		double d1 = Math.pow(arr[0][0]-arr[i1][0], 2) + Math.pow(arr[0][1]-arr[i1][1], 2);
-    		double d2 = Math.pow(arr[i1][0]-arr[mi][0],2) + Math.pow(arr[i1][1] - arr[mi][1],2);
-    		double d3 = Math.pow(arr[mi][0]-arr[i2][0], 2) + Math.pow(arr[mi][1]-arr[i2][1], 2);
-    		double d4 = Math.pow(arr[i2][0]-arr[0][0],2) + Math.pow(arr[i2][1]-arr[0][1], 2);
-    		
-    		double diag1 = Math.pow(arr[0][0]-arr[mi][0], 2) + Math.pow(arr[0][1]-arr[mi][1], 2);
-    		double diag2 = Math.pow(arr[i1][0]-arr[i2][0], 2) + Math.pow(arr[i1][1]-arr[i2][1],2);
-    		
-    		if(d1 == d2 && d2 == d3 && d3 == d4 && d4 == d1 && diag1 == diag2) {
-    			System.out.println(1);
-    		}else {
-    			System.out.println(0);
-    		}
-    	}
-    }
+	public static long dist(long[][] arr, int i, int j) {
+		long dx = arr[i][0] - arr[j][0];
+		long dy = arr[i][1] - arr[j][1];
+
+		return dx * dx + dy * dy;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 0; tc < T; tc++) {
+
+			long[][] arr = new long[4][2];
+
+			for (int i = 0; i < 4; i++) {
+				String[] s = br.readLine().split(" ");
+				arr[i][0] = Long.parseLong(s[0]);
+				arr[i][1] = Long.parseLong(s[1]);
+			}
+
+			long[] d = new long[6];
+			int idx = 0;
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = i + 1; j < 4; j++) {
+					d[idx++] = dist(arr, i, j);
+				}
+			}
+
+			Arrays.sort(d);
+
+			if (d[0] > 0 && d[0] == d[1] && d[1] == d[2] && d[2] == d[3] && d[4] == d[5]) {
+				System.out.println(1);
+			}else {
+				System.out.println(0);
+			}
+		}
+	}
 }
