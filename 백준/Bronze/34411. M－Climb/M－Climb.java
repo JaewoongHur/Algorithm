2636893078e1754a520e00dd5d6cc5ec088e4525
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-11-11 08:39:51 +0900

    [Bronze V] Title: M-Climb, Time: 60 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int m = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+		
+		System.out.println(m * k);
+		
+		
+	}
+}
