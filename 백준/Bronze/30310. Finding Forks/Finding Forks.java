56c6627e13fc58908383e72b1e10ac4e5c120cf7
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-12-02 08:45:00 +0900

    [Bronze III] Title: Finding Forks, Time: 288 ms, Memory: 33888 KB -BaekjoonHub

---
+++ 백준/Bronze/30310. Finding Forks/Finding Forks.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] + arr[1]);
+		
+	}
+}
