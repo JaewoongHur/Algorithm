720323340005a24e38bc4354faf579cf99eaeacb
JQuad <97331332+JaewoongHur@users.noreply.github.com>
2025-12-20 18:37:43 +0900

    [Bronze II] Title: Knights Move, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/34381. Knights Move/Knights Move.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		int a = s.charAt(0) - 'a';
+		int b = s.charAt(1) - '0'- 1;
+		
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<8;j++) {
+				if((Math.abs(a-i) == 2 && Math.abs(b-j)==1) || (Math.abs(a-i)== 1 && Math.abs(b-j) == 2)) {
+					System.out.println(String.valueOf((char)(i+'a')) + String.valueOf(j + 1));
+				}
+			}
+		}
+	}
+}
